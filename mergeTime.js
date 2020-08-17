432801b9c763d5be2e47c4f99dc32c82c2aa2f2a
Bhavna <anjali.verma@charpixel.com>
2020-08-17 12:27:59 +0530

    reArrange the Sentence

--- mergeTime.js
+++ mergeTime.js
@@ -16,13 +16,22 @@ function mergeOverlapIntervals(arr) {
 
    
     let n = arr.sort(compareInterval);
+    console.log("nLength",n.length);     //5
     let IntervelArr = [];
     IntervelArr.push(n[0]);
-    
+    console.log("arrLength",IntervelArr.length);
+    console.log("IntervelArr",IntervelArr); // [ { startTime: 0, endTime: 1 } ]
+                        //5
     for (let i = 1; i < n.length; i++) {
     let top = IntervelArr[IntervelArr.length - 1];
+    // console.log("sdfdeergtfe",IntervelArr[IntervelArr.length - 1]);
+    console.log("top",top);  //initially  strtTme:0, endtme:1
     if (top.endTime < n[i].startTime) {
+        console.log("top.endTime",top.endTime);
+        console.log("n[i].startTime",n[i].startTime);
         IntervelArr.push(n[i]);
+        console.log("IntervelArr111",IntervelArr);
+
     } else if (top.endTime < n[i].endTime) {
     top.endTime = n[i].endTime;
     }
---
+++ reArrngSent.js
@@ -0,0 +1,35 @@
+
+var Sentence = "is2 Thi1s T4est 3a";
+var words = Sentence.split(" ");
+var newArr = [];
+
+
+console.log("updated==========++",newArr);// initially []
+
+for(var i = 0; i < words.length; i++){ 
+    
+    for(var j = 0; j < words[i].length; j++){  
+              
+        if(words[i][j] <= '9' && words[i][j] >= '0'){
+
+            // var wordCount=words[i][j];
+            // console.log("wordCount", wordCount); 
+            // var wordI=words[i];
+            // console.log("wordI", wordI);
+            newArr[words[i][j]] = words[i];
+
+
+       //     newArr[words[i][j]]  <<--------show the index value newArr[2]
+            
+            // console.log("arr value", newArr[i]); 
+        }
+        
+    }
+}
+
+//  console.log("newArr",newArr); // o/p= [ <1 empty item>, 'Thi1s', 'is2', '3a', 'T4est' ]
+
+result = newArr.join(" ");
+var withNoDigits = result.replace(/[0-9]/g, '');
+
+console.log("result",withNoDigits);  // o/p= //Thi1s is2 3a T4est
