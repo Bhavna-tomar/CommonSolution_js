b544ccf16b1309afb121f69c57ec42fbc8668749
Bhavna <anjali.verma@charpixel.com>
2020-08-20 15:54:25 +0530

    console for myObj foo function

---
+++ funcFoo.js
@@ -0,0 +1,43 @@
+
+/*    as using "this" keyword alone as well in regular functions 
+       it referes to the global object/refers to the window object*/
+
+//    ==========>>
+
+//        function sum(){
+//            var add=2+3;
+//            console.log("add", +add);
+//            console.log(this);           //Object [global] or not refer to the regular function 
+//        }
+//    sum();
+
+
+var myObject = { 
+    foo: "bar",    
+     func : function() {    
+        var self =this;  
+         
+        // but here the function is inside an object
+
+    //eg.--->>   console.log(this);
+                 //here this only referes to the owner object like below 
+   // o/p--->  { foo: 'bar', func: [Function: func] } 
+   
+      
+
+        console.log("outer func:  this.foo = " + this.foo);    //
+        console.log("outer func:  self.foo = " + self.foo);    //  it also represent the owner object.
+        (function() {   
+           // it will refer to window object.
+          // In window obj foo is not defined, so it will give the output undefined
+            console.log("inner func:  this.foo = " + this.foo);   
+            console.log("inner func:  self.foo = " + self.foo); 
+
+        /*inner function can have access to the outer function variables 
+           as well as all the global variables*/
+        }());    
+    }    
+};    
+myObject.func();
+
+
--- reArrngSent.js
+++ reArrngSent.js
@@ -4,26 +4,19 @@ var words = Sentence.split(" ");
 var newArr = [];
 
 
-console.log("updated==========++",newArr);// initially []
+console.log("updated==========++",newArr);
 
 for(var i = 0; i < words.length; i++){ 
     
     for(var j = 0; j < words[i].length; j++){  
-              // console.log("words[i].length", words[i].length);
+              
         if(words[i][j] <= '9' && words[i][j] >= '0'){
 
-            // var wordCount=words[i][j];
-            // console.log("wordCount", wordCount); 
-            // var wordI=words[i];
-            // console.log("wordI", wordI);
-            newArr[words[i][j]] = words[i];
-
-
-       //     newArr[words[i][j]]  <<--------show the index value newArr[2]
             
-            // console.log("arr value", newArr[i]); 
+            newArr[words[i][j]] = words[i];       
+           
         }
-        // console.log("arr value", newArr[i]);
+        
     }
 }
 
@@ -32,4 +25,4 @@ for(var i = 0; i < words.length; i++){
 result = newArr.join(" ");
 var resultWithNoDigits = result.replace(/[0-9]/g, '');
 
-console.log("result",resultWithNoDigits);  // o/p= //Thi1s is2 3a T4est
+console.log("result",resultWithNoDigits); 
