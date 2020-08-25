2616a07af6cc27b69b2b5928d537cdb5430458d3
Bhavna <anjali.verma@charpixel.com>
2020-08-25 14:37:44 +0530

    two object comparision

--- objComp.js
+++ objComp.js
@@ -1,35 +1,26 @@
-
- var _ = require('lodash');
-
- const obj1 ={
-      name:"bhavna",
-      age:25,
-      height:9,
- };
+myObj1 = { "name":"Bhavana", "age":"25", "car":null, "friends":['anto', 'rex', 'selva'] }; 
+	myObj2 = { "name":"Bhavana", "age":"30", "car":null }; 
+	 
+	keyObj1 = Object.keys(myObj1); 
+	keyObj2 = Object.keys(myObj2); 
+		 
  
- const obj2 ={
-      name:"bhavna",
-      age:25,
-      height:9,
- };
- 
- console.log(JSON.stringify({a: obj1}) === JSON.stringify({a: obj2})); //true
- console.log(_.isEqual(obj1, obj2));   //true
- 
- //if the sequence of its properties is changed then as the following
- 
- 
- const obj1 ={
-      age:25,
-      height:9,
-      name:"bhavna",
- };
- 
- const obj2 ={
-      name:"bhavna",
-      height:9,
-      age:25,
-      
- };
- console.log(JSON.stringify({a: obj1}) === JSON.stringify({a: obj2}));  //false
- console.log(_.isEqual(obj1, obj2));  //true
+	valueObj1 = Object.values(myObj1); 
+	valueObj2 = Object.values(myObj2); 
+	 
+	 
+	if(keyObj1.length > keyObj2.length) { 
+		var maxSize = keyObj1.length; 
+	} else { 
+		var maxSize = keyObj2.length; 
+	} 
+	 
+	  
+	for(var i=0; i<maxSize; i++) { 
+		if(keyObj1[i] == keyObj2[i] && valueObj1[i] == valueObj2[i]) { 
+			console.log('common value: '+valueObj2[i]);	 
+		} else { 
+			
+			console.log(' different values  myObj1 value: '+ valueObj1[i] + '\ndifferent values myObj2 value: '+ valueObj2[i] +'\n'); 
+		} 
+	}
